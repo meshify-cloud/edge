f90522200a607a5771cc20bddce2822e96021aad
snowinszu <86755838@qq.com>
2025-03-27 18:19:55 +0800

    secure_link.conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 envsubst '${VALID_REFERERS} ${ORIGIN}' < /nginx.conf.template > /etc/nginx/conf.d/default.conf
 
-envsubst '${SECURE_LINK_SECRET}' < /secure_link.conf.template > /etc/nginx/conf.d/secure_link.conf
+envsubst '${SECURE_LINK_SECRET}' < /secure_link.conf.template > /etc/nginx/secure_link.conf
 
 if [ -z "$SECURE_LINK_SECRET" ]; then
     echo "# secure_link 未启用（SECURE_LINK_SECRET 未设置）" > /etc/nginx/conf.d/secure_link.conf
